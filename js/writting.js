85b3fc704e0bf1c8cde6159c297b29ee0a2cfed8
Krzystof Staroń <kisiel3141@gmail.com>
2021-10-07 20:08:50 +0200

    add worlds

--- js/writting.js
+++ js/writting.js
@@ -1,5 +1,11 @@
+let toWrite = ""
+
 const start = () =>{
-  console.log("yes");
+  for (var i = 0; i < 10; i++) {
+    toWrite += words[Math.floor(Math.random() * words.length)] + " "
+  }
+
+  console.log(toWrite);
 }
 
 window.onload = start;
