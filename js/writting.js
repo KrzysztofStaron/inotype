82b52fa7d0b061195306b4a3f3d727cfd80d950b
Krzystof Staroń <kisiel3141@gmail.com>
2021-10-09 15:25:25 +0200

    better comments

--- js/writting.js
+++ js/writting.js
@@ -3,6 +3,7 @@ let toWrite = "";
 let activeId = 0;
 let endPoints = [];
 
+// Generate Text Function
 const generate = () => {
   // Clear
   toWrite = "";
@@ -17,11 +18,12 @@ const generate = () => {
     const word = words[Math.floor(Math.random() * words.length)];
     toWrite += word;
     const letters = word.split("");
-    for (var q = 0; q < letters.length; q++) {
+    for (var q = 0; q < letters.length; q ++) {
       document.getElementById(`word${i}`).innerHTML += '<input onkeyup="check(this)" type="text" class="letter" id=letter'+letterId+' maxlength="1" placeholder='+letters[q]+'>';
       letterId++;
     }
   }
+
   // Set Focus At First Letter
   document.getElementById("letter0").focus();
 }
@@ -29,19 +31,21 @@ window.onload = generate;
 
 // Checking The Correctness Of The Char
 const check = (obj) => {
+  activeId++;
+
   // Set Color
   if (toWrite[obj.id.slice(6,obj.id.length)] == obj.value) {
     obj.style.color = "green";
   } else {
     obj.style.color = "red";
   }
+
   // Loop
-  if (!document.getElementById('letter' + activeId+1)) {
+  if (!document.getElementById('letter' + activeId)) {
     generate();
     return ;
   }
 
   // Set Focus To Next Input
-  activeId++;
   document.getElementById('letter'+activeId).focus();
 }
