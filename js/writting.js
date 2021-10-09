f0df04fd12a7911e5f7d13fb803b7e051640c13c
Krzystof Staroń <kisiel3141@gmail.com>
2021-10-09 11:11:04 +0200

    better generation

--- js/writting.js
+++ js/writting.js
@@ -6,13 +6,17 @@ const start = () => {
 }
 
 const generate = () => {
+  toWrite = "";
+  let letterId = 0;
   document.getElementById('text').innerHTML = "";
   for (var i = 0; i < 10; i++) {
     document.getElementById('text').innerHTML += '<div class="word" id=word'+i+'></div>';
     const word = words[Math.floor(Math.random() * words.length)];
+    toWrite += word;
     const letters = word.split("");
     for (var q = 0; q < letters.length; q++) {
-      document.getElementById(`word${i}`).innerHTML += '<input type="text" class="letter" maxlength="1" placeholder='+letters[q]+'>';
+      document.getElementById(`word${i}`).innerHTML += '<input type="text" class="letter" id=letter'+letterId+' maxlength="1" placeholder='+letters[q]+'>';
+      letterId++;
     }
   }
 }
