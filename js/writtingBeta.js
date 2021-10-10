2f34fe43980108e875ec2f60d683701c7bfa7707
Krzystof Staroń <kisiel3141@gmail.com>
2021-10-10 10:02:26 +0200

    beta

--- js/writtingBeta.js
+++ js/writtingBeta.js
@@ -25,7 +25,12 @@ const generate = () => {
     document.getElementById('text').innerHTML += '<word id=w'+w+'></word>'
 
     word.split("").forEach((item) => {
-      document.getElementById('w'+w).innerHTML += '<letter id=l'+letterId+' class="" >'+item+'</letter>';
+      if (item != " ") {
+        document.getElementById('w'+w).innerHTML += '<letter id=l'+letterId+' class="" >'+item+'</letter>';
+      } else {
+        document.getElementById('w'+w).innerHTML += '<letter id=l'+letterId+' name="space">'+item+'</letter>';
+      }
+
       letterId++;
     });
     if (w%worldsInLine == 0 && w > 0) {
@@ -47,20 +52,28 @@ const logKey = (e) => {
     if (activeId-1 < 0) {
       return ;
     }
+    if (document.getElementById('l'+activeId).name == "space") {
+      document.getElementById('l'+activeId).innerHTML = " ";
+    }
     document.getElementById('l'+activeId).className = "";
     activeId--;
     document.getElementById('l'+activeId).className = "active";
     return ;
   }
   if((e.keyCode >= 65 && e.keyCode <= 90) || char == " ") {
+    if (document.getElementById('l'+activeId).name == "space") {
+      document.getElementById('l'+activeId).innerHTML = " ";
+    }
     if (char == toWrite[activeId]) {
       document.getElementById('l'+activeId).className = "correct";
       writenChars++;
     }else {
       document.getElementById('l'+activeId).className = "wrong";
       writenChars--;
-      if (toWrite[activeId]) {
-
+      if (toWrite[activeId] == " ") {
+        document.getElementById('l'+activeId).className = "space";
+        document.getElementById('l'+activeId).name = "space";
+        document.getElementById('l'+activeId).innerHTML = e.key;
       }
     }
     activeId++;
