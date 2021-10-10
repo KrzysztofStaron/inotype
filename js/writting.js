011eede5ac0f9d341c9c457943d44064e3b785d1
Krzystof Staroń <kisiel3141@gmail.com>
2021-10-10 10:54:43 +0200

    beta is normal

--- js/writting.js
+++ js/writting.js
@@ -1,94 +1,99 @@
-// Variables
 let toWrite = "";
 let activeId = 0;
-let endPoints = [];
+let writenChars = 0;
+let time = 60.0;
 
-// Generate Text Function
-const generate = () => {
-  // Clear
-  toWrite = "";
-  activeId = 0;
-  endPoints = [];
-  let letterId = 0;
-  document.getElementById('text').innerHTML = "";
-
-  // Generate Text
-  for (var i = 0; i < 10; i++) {
-    document.getElementById('text').innerHTML += '<div class="word" id=word'+i+'></div>';
-    let word = words[Math.floor(Math.random() * words.length)];
-    if (i != 9) {
-      word += " ";
-    }
-    toWrite += word;
-    const letters = word.split("");
-    for (var q = 0; q < letters.length; q ++) {
-      document.getElementById(`word${i}`).innerHTML += '<input onkeyup="check(this)" onfocus="checkFocusLegality(this)" type="text" class="letter" id=letter'+letterId+' maxlength="1" placeholder='+letters[q]+'>';
-      letterId++;
-    }
-  }
-
-  // Set Focus At First Letter
-  setFocus(activeId);
-}
-
-const start = () =>{
+const start = () => {
   document.addEventListener('keydown', logKey);
   generate();
 }
+
+var tick = setInterval(function() {
+  if (time <= 0) {
+    clearInterval(tick)
+    time = 0;
+    document.getElementById('timer').innerHTML = "0";
+  }
+  time -= 0.1
+  time = Math.round(time * 10) / 10;
+  if (time%1 == 0) {time += ".0"}
+  document.getElementById('timer').innerHTML = time;
+}, 100)
+
 window.onload = start;
 
-const check = (obj, force = false) => {
-  // Check For Illegar Char
-  if (!obj.value) {
-    obj.value = "";
-    console.error("Illegal Char");
-    return ;
-  } else if (force) {
-    obj.value = toWrite[activeId];
-  }
+const generate = () => {
+  toWrite = "";
+  activeId = 0;
+  let letterId=0;
+  const textLength = 15;
+  const worldsInLine=5;
+  document.getElementById('text').innerHTML = '';
+  for (var w = 0; w < textLength; w++) {
+    let word = words[Math.floor(Math.random() * words.length)];
+    if ((w%worldsInLine != 0 || w == 0) && w != textLength-1){
+      word += " ";
+    }
+    toWrite += word;
+    document.getElementById('text').innerHTML += '<word id=w'+w+'></word>'
 
-  // Set Color And Check Char Correctness
-  if (toWrite[activeId] == obj.value) {
-    obj.style.color = "green"; // Correct Char
-  } else {
-    obj.style.color = "red"; // Wrong Char
-  }
-  activeId++;
+    word.split("").forEach((item) => {
+      if (item != " ") {
+        document.getElementById('w'+w).innerHTML += '<letter id=l'+letterId+' class="" >'+item+'</letter>';
+      } else {
+        document.getElementById('w'+w).innerHTML += '<letter id=l'+letterId+' name="space">'+item+'</letter>';
+      }
 
-  // Loop
-  if (activeId < 0) {
-    activeId = 0;
+      letterId++;
+    });
+    if (w%worldsInLine == 0 && w > 0) {
+      document.getElementById('text').innerHTML += '<br>'
+    }
   }
-  if (!document.getElementById('letter' + activeId)) {
+  document.getElementById('l0').className = "active";
+  toWrite = toWrite.toLowerCase()
+  console.log(toWrite);
+}
+
+const logKey = (e) => {
+  const char = e.key.toLowerCase();
+  if (activeId == toWrite.length-1) {
     generate();
     return ;
   }
-
-  // Set Focus To Next Input
-  setFocus(activeId);
-}
-
-// Detect Special keys fe. Backspace
-const logKey = (e) => {
-  // Fix Special Keys
-  if ((e.keyCode >= 41 && e.keyCode <= 90) || (e.keyCode >= 97 && e.keyCode <= 122)) {
-    check(document.getElementById('letter' + activeId), true)
-  }
   if (e.code == "Backspace") {
+    if (activeId-1 < 0) {
+      return ;
+    }
+    if (document.getElementById('l'+activeId).name == "space") {
+      document.getElementById('l'+activeId).innerHTML = " ";
+    }
+    document.getElementById('l'+activeId).className = "";
     activeId--;
-    setFocus(activeId)
+    document.getElementById('l'+activeId).className = "active";
+    return ;
   }
-}
-
-// Fix Focus
-const checkFocusLegality = (obj) => {
-  if (obj.id != "letter"+activeId){
-    setFocus(activeId);
-    console.error("Illegal Focus Detected");
+  if((e.keyCode >= 65 && e.keyCode <= 90) || char == " ") {
+    if (document.getElementById('l'+activeId).name == "space") {
+      document.getElementById('l'+activeId).innerHTML = " ";
+    }
+    if (char == toWrite[activeId]) {
+      document.getElementById('l'+activeId).className = "correct";
+      writenChars++;
+    }else {
+      document.getElementById('l'+activeId).className = "wrong";
+      if (writenChars-1 >= 0) {
+        writenChars--;
+      }
+      if (toWrite[activeId] == " ") {
+        document.getElementById('l'+activeId).className = "space";
+        document.getElementById('l'+activeId).name = "space";
+        document.getElementById('l'+activeId).innerHTML = e.key;
+      }
+    }
+    activeId++;
+    document.getElementById('l'+activeId).className = "active";
+    //e == 69
   }
-}
 
-// Set Input Focus
-const setFocus = (id = activeId) => {
-  document.getElementById('letter' + id).focus();
 }
